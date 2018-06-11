1b38fc897d8365c82b3f8148db516fb59afc6eec
toy.zhang <1313_zxj@sina.com>
2018-06-11 18:15:49 +0800

    update

--- src/main/java/com/jshop/jshop/application/JshopApplication.java
+++ src/main/java/com/jshop/jshop/application/JshopApplication.java
@@ -3,14 +3,23 @@ package com.jshop.jshop.application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jshop.jshop.*"})
 @MapperScan(basePackages = {"com.jshop.jshop.mapper"})
-public class JshopApplication {
+public class JshopApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JshopApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JshopApplication.class);
+    }
+
+
 }
